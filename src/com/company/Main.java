66ca2ab6dd9fdca402eee861904354665cef2a62
unknown <greatabhi.abhishek@gmail.com>
2019-08-22 06:18:53 +0530

    First Java Project

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.awt.*;
+import java.security.Provider;
+import java.security.Security;
+import java.util.Date;
+
+public class Main {
+    public static void main (String[] args) {
+        Point point1 = new Point(1 , 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
+
+
+    }
+
+}
